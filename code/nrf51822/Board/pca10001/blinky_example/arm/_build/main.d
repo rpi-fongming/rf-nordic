b8cd0ce12a692d1db15f2737bffc1f7af6c8574d
rpi-fongming <fm2013.fongshek@gmail.com>
2013-10-07 10:16:43 +0800

    Add nrf51822 project

---
+++ code/nrf51822/Board/pca10001/blinky_example/arm/_build/main.d
@@ -0,0 +1,17 @@
+.\_build\main.o: ..\main.c
+.\_build\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\_build\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\_build\main.o: ..\..\..\..\Include\nrf_delay.h
+.\_build\main.o: ..\..\..\..\Include\nrf.h
+.\_build\main.o: ..\..\..\..\Include\compiler_abstraction.h
+.\_build\main.o: ..\..\..\..\Include\nrf51.h
+.\_build\main.o: C:\Keil\ARM\CMSIS\Include\core_cm0.h
+.\_build\main.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\_build\main.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\_build\main.o: ..\..\..\..\Include\system_nrf51.h
+.\_build\main.o: ..\..\..\..\Include\nrf51_bitfields.h
+.\_build\main.o: C:\Keil\ARM\CMSIS\Include\core_cm0.h
+.\_build\main.o: ..\..\..\..\Include\nrf51_deprecated.h
+.\_build\main.o: ..\..\..\..\Include\nrf_gpio.h
+.\_build\main.o: ..\..\..\..\Include\boards.h
+.\_build\main.o: ..\..\..\..\Include\boards/pca10001.h
---
+++ code/nrf51822/Board/pca10001/blinky_example/arm/_build/startup_arm_nrf51.d
@@ -0,0 +1,1 @@
+.\_build\startup_arm_nrf51.o: ..\..\..\..\Source\templates\arm\arm_startup_nrf51.s
